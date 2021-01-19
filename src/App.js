ec86d39e011b4e4ac5da9da41f215eb6422fd19c
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-01-19 21:39:19 +0100

    added tasks and icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'Docs Appointment',
+			day: 'Feb 5th at 2:30pm',
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: 'School Meeting',
+			day: 'Feb 6th at 1:30pm',
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: 'Food Shopping',
+			day: 'Feb 5th at 2:30pm',
+			reminder: false,
+		},
+	]);
+
 	return (
 		<div className='container'>
 			<Header />
+			<Tasks tasks={tasks} />
 		</div>
 	);
 }
